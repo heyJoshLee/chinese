1b14df1d879f5917e1eca10218a83be2e2dbefd4
Josh Lee <Lee45276@gmail.com>
2015-08-20 00:43:01 +0800

    Created simple form to create words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,9 +1,31 @@
 class WordsController < ApplicationController
 
+  def index
+    @words = Word.all
+  end
+
   def show
   end
 
   def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_parmas)
+
+    if @word.save 
+      redirect_to words_path
+    else
+      redirect_to :back
+    end
+
+  end
+
+  private
+
+  def word_parmas
+    params.require(:word).permit!
   end
 
 end
